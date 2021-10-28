615d9a1debccf362cc06295fc30711ddd6f41e50
Charles <crg0905@gmail.com>
2021-10-27 18:02:25 -0700

    implement firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDy_aFVQWY8xRRpcHnB5RK_-vYUQeIMZZo",
+    authDomain: "fib-seq-test.firebaseapp.com",
+    projectId: "fib-seq-test",
+    storageBucket: "fib-seq-test.appspot.com",
+    messagingSenderId: "861915641295",
+    appId: "1:861915641295:web:5b36cba828f2d2a41f4eba"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,23 +3,28 @@ import './Input.css';
 
 function Input() {
 
-
+    const [start, setStart] = useState(0);
+    const [finish, setFinish] = useState(0);
 
     //take 2 numbers from user ("start" & "finish")
     //print fib sequence until finish
     //loop to conosle log output using start as starting point and finish as ending point
 
 
+    function handleSubmit(e) {
+        setStart(e.input.value);
+        console.log(start);
+    }
 
     return (
             <div>
                 <div className="box">
                     <h3>Please enter two numbers below:</h3>
                     <div className="calculations">
-                        <input placeholder="Enter a number to start!" active />
+                        <input placeholder="Enter a number to start!" />
                         <input  placeholder="Enter another number to finish!" />
                     </div>
-                    <button type="button" >GO!</button>
+                    <button type="submit" onSubmit={handleSubmit}>GO!</button>
                     
                 </div>
                 
