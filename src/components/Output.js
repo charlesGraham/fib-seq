4307a45f568f5ce6847279757354580276ed16f9
Charles <crg0905@gmail.com>
2021-10-27 20:53:15 -0700

    catch all

--- src/components/Output.js
+++ src/components/Output.js
@@ -11,33 +11,31 @@ function Output({ data }) {
     function fibSeq(number) {
         let num1 = 0;
         let num2 = 1;
+        let nextNum;
         
         for(let i = 0; i <= number; i++) {
+            //test nextNum
+            if (nextNum === data.secondVal) {
+                break;
+            }
+
             console.log(num1);
-            let nextNum;
-    
             nextNum = num1 + num2;
             num1 = num2;
             num2 = nextNum;
-
-            //test nextNum
-            if (nextNum === data.secondVal) {
-                output.push(nextNum);
-                return output;
-            } else {
-                output.push(nextNum);
-            }
         }
+
+        return output;
     } 
 
     console.log(data);
-    fibSeq(10);
+    fibSeq(data.secondVal);
     console.log(output);
 
     return (
         <div className="box">
             <h3>The Fibonacci sequence numbers between the two you entered are:</h3>
-            <p></p>
+            <p>{data.secondVal}</p>
 
         </div>
 )
