81dffdd38a56bf64155c05b8f52104bb0ac3fd28
Charles <crg0905@gmail.com>
2021-10-27 18:11:50 -0700

    logic for <Input /> to get input from text fields

--- src/components/Input.js
+++ src/components/Input.js
@@ -11,9 +11,12 @@ function Input() {
     //loop to conosle log output using start as starting point and finish as ending point
 
 
-    function handleSubmit(e) {
-        setStart(e.input.value);
-        console.log(start);
+    function getStart(e) {
+        setStart(e.target.value);
+    }
+
+    function getFinish(e) {
+        setFinish(e.target.value);
     }
 
     return (
@@ -21,10 +24,10 @@ function Input() {
                 <div className="box">
                     <h3>Please enter two numbers below:</h3>
                     <div className="calculations">
-                        <input placeholder="Enter a number to start!" />
-                        <input  placeholder="Enter another number to finish!" />
+                        <input type="text" onChange={getStart} placeholder="Enter a number to start!" />
+                        <input type="text" onChange={getFinish}  placeholder="Enter another number to finish!" />
                     </div>
-                    <button type="submit" onSubmit={handleSubmit}>GO!</button>
+                    <button type="submit">GO!</button>
                     
                 </div>
                 
